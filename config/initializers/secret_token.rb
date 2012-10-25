9ca97a33a3f42bc0953a3e2a7a13149d5bab1c14
Timo Uhlmann <timouhlmann@googlemail.com>
2012-10-25 23:01:26 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Putter::Application.config.secret_token = '48d25b3f20efa4823337beb1c817f2277c2e9e3cdaff310a826ac3d747e2a54b60d9e37081763f5d47fdd28bc6b5d1e8745af6cec5d2870ad112686b4f7bc53f'
